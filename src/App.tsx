4a75840ec5a082c407a745ecb195e10fb2821afc
swj9707 <swj1265@naver.com>
2022-10-02 20:18:44 +0900

    feature/client-todolist basic CRUD

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ItemInterface from './Store/Type/ItemInterface';
+import {Paper, List, Container} from "@mui/material"
+import Todo from './component/Todo';
+import AddTodo from './component/AddTodo';
 
 function App() {
+  const [items, setItems] = useState<ItemInterface[]>([]);
+ 
+
+  const addItem = (item : ItemInterface) => {
+    item.id = "ID-" + items.length;
+    item.done = false
+    setItems([...items, item]);
+    console.log("items : ", items);
+  }
+
+  const deleteItem = (item : ItemInterface) => {
+    const newItems = items.filter(e => e.id !== item.id);
+    setItems([...newItems]);
+  }
+
+  const editItem = () =>{
+    setItems([...items])
+  }
+
+  let todoItems = items.length > 0 && (
+    <Paper style={{margin : 16}}>
+        <List>
+          {items.map((item) => (
+            <Todo item = {item} key = {item.id} editItem={editItem} deleteItem={deleteItem}></Todo>
+          ))}
+        </List>
+    </Paper>
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container maxWidth="md">
+        <AddTodo addItem={addItem}/>
+        <div className="TodoList">{todoItems}</div>  
+      </Container>
     </div>
   );
 }
---
+++ src/Store/Type/ItemInterface.ts
@@ -0,0 +1,5 @@
+export default interface ItemInterface{
+    id : string;
+    title : string;
+    done : boolean;
+}
